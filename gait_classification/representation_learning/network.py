ddfc07b7cb9b4b7e6ad7d2be963e68f2f3d4b2f3
Jonathan Schwarz <schwarzjn@gmail.com>
2016-03-11 11:15:32 +0000

    added denoising stacked convolutional autoencoder

---
+++ gait_classification/representation_learning/network.py
@@ -0,0 +1,28 @@
+import numpy as np
+import theano
+import theano.tensor as T
+
+from nn.ActivationLayer import ActivationLayer
+from nn.NoiseLayer import NoiseLayer
+from nn.Pool1DLayer import Pool1DLayer
+from nn.Conv1DLayer import Conv1DLayer
+from nn.Network import Network, AutoEncodingNetwork
+
+rng = np.random.RandomState(23455)
+
+network = Network(
+
+    NoiseLayer(rng, 0.3),
+    
+    Conv1DLayer(rng, (64, 66, 25), (1, 66, 240)),
+    Pool1DLayer(rng, (2,), (1, 64, 240)),
+    ActivationLayer(rng),
+
+    Conv1DLayer(rng, (128, 64, 25), (1, 64, 120)),
+    Pool1DLayer(rng, (2,), (1, 128, 120)),
+    ActivationLayer(rng),
+    
+    Conv1DLayer(rng, (256, 128, 25), (1, 128, 60)),
+    Pool1DLayer(rng, (2,), (1, 256, 60)),
+    ActivationLayer(rng)
+)
