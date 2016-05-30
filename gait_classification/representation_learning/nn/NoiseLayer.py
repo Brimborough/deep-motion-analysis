2e735834b103b3330d9ff5d60aaa450465a62cbb
Jonathan Schwarz <SchwarzJn@googlemail.com>
2016-05-30 13:15:12 +0100

    added Gaussian Noise layer

--- gait_classification/representation_learning/nn/NoiseLayer.py
+++ gait_classification/representation_learning/nn/NoiseLayer.py
@@ -24,3 +24,22 @@ class NoiseLayer(object):
     def load(self, filename): pass
     def save(self, filename): pass
         
+class GaussianNoiseLayer(object):
+
+    def __init__(self, rng, sigma=1.0):
+        self.sigma = sigma
+        self.theano_rng = RandomStreams(rng.randint(2 ** 30))
+        self.params = []
+        
+    def __call__(self, input):
+        if self.sigma > 0.0:
+            return (input + self.theano_rng.normal(
+                size=input.shape, dtype=theano.config.floatX))
+        else:
+            return input
+        
+    def inv(self, output):
+        return output
+        
+    def load(self, filename): pass
+    def save(self, filename): pass
