54df9dbd10d3ec17dbbf1fc50854db34ddd1ae44
Jonathan Schwarz <SchwarzJn@googlemail.com>
2016-06-08 18:29:32 +0100

    adopted to new definition in utils

--- gait_classification/representation_learning/train_cnn.py
+++ gait_classification/representation_learning/train_cnn.py
@@ -19,19 +19,23 @@ rng = np.random.RandomState(23455)
 dataset = '../data/mnist/mnist.pkl.gz'
 datasets = load_data(dataset)
 
-train_set_x, train_set_y = datasets[0]
-valid_set_x, valid_set_y = datasets[1]
-test_set_x, test_set_y = datasets[2]
+shared = lambda d: T.shared(d, borrow=True)
 
-train_set_x = train_set_x.reshape((50000,1,28,28))
-valid_set_x = valid_set_x.reshape((10000,1,28,28))
-test_set_x = test_set_x.reshape((10000,1,28,28))
+train_set_x, train_set_y = map(shared, datasets[0])
+valid_set_x, valid_set_y = map(shared, datasets[1])
+test_set_x, test_set_y   = map(shared, datasets[2])
+
+batchsize = 1
+
+train_set_x = train_set_x.reshape((50000, 1, 28, 28))
+valid_set_x = valid_set_x.reshape((10000, 1, 28, 28))
+test_set_x  = test_set_x.reshape((10000, 1, 28, 28))
 
 network = Network(
 	NoiseLayer(rng, 0.3),
 
-	Conv2DLayer(rng, (4, 1, 5, 5), (1, 1, 28, 28)),
-	Pool2DLayer(rng, (1, 4, 28, 28)),
+	Conv2DLayer(rng, (4, 1, 5, 5), (batchsize, 1, 28, 28)),
+	Pool2DLayer(rng, (batchsize, 4, 28, 28)),
 	ActivationLayer(rng, f='ReLU'),
 	ReshapeLayer(rng, (4*14*14, )),
 
@@ -39,7 +43,7 @@ network = Network(
 	ActivationLayer(rng, f='softmax')
 )
 
-trainer = AdamTrainer(rng=rng, batchsize=1, epochs=5, alpha=0.00001, cost='cross_entropy')
+trainer = AdamTrainer(rng=rng, batchsize=batchsize, epochs=5, alpha=0.00001, cost='cross_entropy')
 trainer.train(network=network, train_input=train_set_x, train_output=train_set_y,
                                valid_input=valid_set_x, valid_output=valid_set_y,
                                test_input=test_set_x, test_output=test_set_y, filename=None)
