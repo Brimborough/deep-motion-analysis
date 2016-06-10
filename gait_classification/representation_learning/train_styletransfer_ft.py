cc855929509effe58aa01da5c750e25591df3b8d
Jonathan Schwarz <SchwarzJn@googlemail.com>
2016-06-10 12:59:17 +0100

    styletransfer experiments with batchnorm

--- gait_classification/representation_learning/train_styletransfer_ft.py
+++ gait_classification/representation_learning/train_styletransfer_ft.py
@@ -5,14 +5,14 @@ import theano.tensor as T
 import sys
 sys.path.append('../representation_learning/')
 
-#from nn.ActivationLayer import ActivationLayer
-#from nn.AdamTrainer import AdamTrainer
-#from nn.BatchNormLayer import BatchNormLayer
-#from nn.Conv1DLayer import Conv1DLayer
-#from nn.HiddenLayer import HiddenLayer
-#from nn.Network import Network
-#from nn.Pool1DLayer import Pool1DLayer
-#from nn.ReshapeLayer import ReshapeLayer
+from nn.ActivationLayer import ActivationLayer
+from nn.AdamTrainer import AdamTrainer
+from nn.BatchNormLayer import BatchNormLayer
+from nn.Conv1DLayer import Conv1DLayer
+from nn.HiddenLayer import HiddenLayer
+from nn.Network import Network
+from nn.Pool1DLayer import Pool1DLayer
+from nn.ReshapeLayer import ReshapeLayer
 
 rng = np.random.RandomState(23455)
 
@@ -59,34 +59,39 @@ Y_valid = theano.shared(np.array(Y)[cv_split:test_split], borrow=True)
 X_test = theano.shared(np.array(X)[test_split:], borrow=True)
 Y_test = theano.shared(np.array(Y)[test_split:], borrow=True)
 
-network = Network(
-    Conv1DLayer(rng, (64, 66, 25), (1, 66, 240)),
-    Pool1DLayer(rng, (2,), (1, 64, 240)),
-    ActivationLayer(rng, f='ReLU'),
+batchsize = 2
 
-    Conv1DLayer(rng, (128, 64, 25), (1, 64, 120)),
-    Pool1DLayer(rng, (2,), (1, 128, 120)),
+network = Network(
+    Conv1DLayer(rng, (64, 66, 25), (batchsize, 66, 240)),
+    BatchNormLayer(rng, (batchsize, 64, 240)),
     ActivationLayer(rng, f='ReLU'),
+    Pool1DLayer(rng, (2,), (batchsize, 64, 240)),
+
+    Conv1DLayer(rng, (128, 64, 25), (batchsize, 64, 120)),
+    BatchNormLayer(rng, (batchsize, 128, 120)),
+    ActivationLayer(rng, f='ReLU'),
+    Pool1DLayer(rng, (2,), (batchsize, 128, 120)),
     
-    Conv1DLayer(rng, (256, 128, 25), (1, 128, 60)),
-    Pool1DLayer(rng, (2,), (1, 256, 60)),
+    Conv1DLayer(rng, (256, 128, 25), (batchsize, 128, 60)),
+    BatchNormLayer(rng, (batchsize, 256, 60)),
     ActivationLayer(rng, f='ReLU'),
+    Pool1DLayer(rng, (2,), (batchsize, 256, 60)),
 
     # Final pooling gives 1, 256, 30 
 
     # 256*60 = 7680
-    ReshapeLayer(rng, (7680, )),
-    HiddenLayer(rng, (np.prod([1, 256, 30]), 8)),
+    ReshapeLayer(rng, (batchsize, 7680)),
+    HiddenLayer(rng, (np.prod([256, 30]), 8)),
     ActivationLayer(rng, f='softmax'),
 )
 
 # Load the pre-trained conv-layers
-network.load(['../models/layer_0.npz', None, None,
-              '../models/layer_1.npz', None, None,
-              '../models/layer_2.npz', None, None, 
+network.load(['../models/conv_ae/layer_0.npz', None, None,
+              '../models/conv_ae/layer_1.npz', None, None,
+              '../models/conv_ae/layer_2.npz', None, None, 
               None, None, None])
 
-trainer = AdamTrainer(rng=rng, batchsize=1, epochs=1, alpha=0.1, cost='cross_entropy')
+trainer = AdamTrainer(rng=rng, batchsize=batchsize, epochs=1, alpha=0.00001, cost='cross_entropy')
 
 # Fine-tuning for classification
 trainer.train(network=network, train_input=X_train, train_output=Y_train, 
@@ -94,10 +99,3 @@ trainer.train(network=network, train_input=X_train, train_output=Y_train,
                                test_input=X_test, test_output=Y_test,
                                # Don't save the params
                                filename=[None] * len(network.layers))
-
-# Don't save anyting for now
-#filename=[None,
-#'layer_0_finetuned.npz', None, None,
-#'layer_1_finetuned.npz', None, None,
-#'layer_2_finetuned.npz', None, None,
-#None, 'layer_3_finetuned.npz', None,])
