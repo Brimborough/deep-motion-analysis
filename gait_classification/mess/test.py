d074508d3c3983558c67fe4751bb0bbd770feee1
josephyearsley <joggino23@gmail.com>
2016-06-13 19:42:43 +0100

    Added lots to the LSTM, added save and load functions to batchnorm, and made the BLSTM layer

--- gait_classification/mess/test.py
+++ gait_classification/mess/test.py
@@ -4,18 +4,48 @@ import pprint
 from theano.compat.python2x import OrderedDict
 import theano.tensor as T
 from theano.tensor.shared_randomstreams import RandomStreams
+import sys
+
+sys.path.append('../representation_learning/nn')
+
+from BatchNormLayer import BatchNormLayer
 
 
-'''
-x = np.array([[ 1, 2, 3],
-     [ 4, 5, 6]])
+class bn(object):
 
-y = np.array([[1, 2], [3, 4], [5,6]])
+    def createBN(self, bn, batch_size, input_shape, hidden_shape):
+        data = OrderedDict()
+        for i in ['input','hidden','cell']:
+            axes = False
+            epsilon = False
 
-print x.shape
-print y.shape
-print T.dot(x,y).eval()
+            if i == 'input':
+                shape = input_shape
+            else:
+                shape = hidden_shape
 
-'''
+            if i+'_axes' in bn:
+                axes =  bn[i+'_axes']
+            if i + '_epsilon' in bn:
+                epsilon =  bn[i + '_epsilon']
 
-# Test how outcome of vector dot matrix
+            if axes and epsilon:
+                print epsilon
+                data[i] = BatchNormLayer(None, [batch_size, shape], axes=axes, epsilon=epsilon)
+            elif axes:
+                print axes
+                data[i] = BatchNormLayer(None, [batch_size, shape], axes=axes)
+            elif epsilon:
+                data[i] = BatchNormLayer(None, [batch_size, shape], epsilon=epsilon)
+            else:
+                data[i] = BatchNormLayer(None, [batch_size, shape])
+
+        return data.values()
+
+b = bn()
+
+c,d,n = b.createBN({'input_axes':(2,)},2,2,2)
+
+print c
+
+print d.epsilon
