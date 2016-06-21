c6a9de1fbe3c9d05072866a629190a32c9c513dd
Jonathan Schwarz <SchwarzJn@googlemail.com>
2016-06-21 14:23:24 +0100

    added tests for join

--- gait_classification/representation_learning/nn_unit_tests/misc_tests.py
+++ gait_classification/representation_learning/nn_unit_tests/misc_tests.py
@@ -18,7 +18,9 @@ Y = T.dmatrix()
 
 label   = lambda X, Y: X[T.nonzero(Y)[0]]
 unlabel = lambda X, Y: X[T.nonzero(1.-T.sum(Y, axis=1))]
+
 split   = lambda X, Y: [label(X, Y), unlabel(X,Y)]
+join    = lambda X, Y: T.concatenate([X, Y], axis=0)
 
 # Predictions
 pred    = lambda Y: T.argmax(Y, axis=1)
@@ -26,16 +28,30 @@ pred    = lambda Y: T.argmax(Y, axis=1)
 label_func   = theano.function([X, Y], label(X, Y))
 unlabel_func = theano.function([X, Y], unlabel(X, Y))
 split_func   = theano.function([X, Y], split(X,Y))
+join_func    = theano.function([X, Y], join(X,Y))
 
 # Make sure error is only calculated for labeled data
 #error_func = theano.function([X, Y], T.mean(T.neq(T.argmax(label(X, Y), axis=1), T.argmax(label(Y, Y), axis=1))))
 error_func = theano.function([X, Y], T.mean(T.neq(pred(label(X, Y)), pred(label(Y, Y)))))
 
-l, u = split_func(x,y)
+l_input, u_input   = split_func(x,y)
+l_output, u_output = split_func(y,y)
 
-print 'labeled:'
-print l
-print 'unlabeled:'
-print u
+print 'original:'
+print x
+print y
+
+print 'input:'
+print l_input
+print u_input
+
+print 'output:'
+print l_output
+print u_output
+
+print 'joint input:'
+print join_func(l_input, u_input)
+print 'joint output:'
+print join_func(l_output, u_output)
 print 'error:'
 print error_func(x, y)
