4c6c0da66f703143fd508c8eb288dae593580130
jy <deep@deep>
2016-07-06 18:21:46 +0100

    Update

--- gait_classification/joe_keras_work/sequential_final_frame.py
+++ gait_classification/joe_keras_work/sequential_final_frame.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, TimeDistributed
 from keras.layers import LSTM
+from keras.optimizers import Nadam
 from keras.utils.data_utils import get_file
 import numpy as np
 import random
@@ -53,7 +54,9 @@ model.add(Dropout(0.2))
 model.add(TimeDistributed(Dense(256)))
 model.add(Activation('tanh'))
 # TimedistributedDense on top - Can then set output vectors to be next sequence!
-model.compile(loss='mean_squared_error', optimizer='nadam')
+
+model.compile(loss='mean_squared_error', optimizer='adam')
+
 
 print('Training model...')
 hist = model.fit(train_x, train_y, batch_size=5, nb_epoch=50, validation_data=(test_x,test_y))
@@ -63,6 +66,7 @@ print(hist.history)
 #TODO - replace the final 5 frames and see how it does
 #TODO: - Make 2 files, time distributed and just final outputs, as well as 2 input files. Shapes are slightly different.
 
+'''
 for i in range(1):
     preds = model.predict(train_x)[:,-1] # SHAPE - [321,29,256], want final prediction, use -1 for time distributed.
     train_x = np.expand_dims(data_util(preds,train_x),0) #Place together all, then only use the final one
@@ -70,7 +74,7 @@ for i in range(1):
 d2 = train_x.swapaxes(0, 1) #Swap back, need to concat again?!
 dat = d2 #For time distributed
 dat = np.expand_dims(d2, 0) #For dense, since it cuts off a dim otherwise
-
+'''
 '''
 from network import network
 network.load([
