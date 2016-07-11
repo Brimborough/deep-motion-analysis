bce437a335cfdbf976c4e4fa5b4641efc7d0accc
josephyearsley <joggino23@gmail.com>
2016-07-11 23:32:56 +0100

    added spearmint runs

--- gait_classification/joe_keras_work/experiments/2_GRU/2GRU-0.6.py
+++ gait_classification/joe_keras_work/experiments/2_GRU/2GRU-0.6.py
@@ -59,8 +59,9 @@ model.compile(loss='mean_squared_error', optimizer='nadam')
 
 
 print('Training model...')
-hist = model.fit(train_x, train_y, batch_size=10, nb_epoch=50, validation_data=(test_x,test_y))
+hist = model.fit(train_x, train_y, batch_size=10, nb_epoch=50, callbacks=[keras.callbacks.EarlyStopping()], \
+                    validation_data=(test_x,test_y))
 print(hist.history)
 score = model.evaluate(test_x,test_y)
 print(score)
-model.save_weights('../../weights/2LSTM-0.6.hd5')
+model.save_weights('../../weights/2GRU-0.6.hd5')
