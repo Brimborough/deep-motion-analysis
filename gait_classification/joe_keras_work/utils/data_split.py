5fa7b59984d69dff30347d315c1f8cb9281b095c
jy <deep@deep>
2016-07-14 20:48:30 +0100

    Merged hdm05 with edin

--- gait_classification/joe_keras_work/utils/data_split.py
+++ gait_classification/joe_keras_work/utils/data_split.py
@@ -8,17 +8,17 @@ class split_data:
 
 
         # Remove old files
-        if(os.path.isfile('../../data/Joe/pre_proc_lstm.npz')):
-            os.remove('../../data/Joe/pre_proc_lstm.npz')
-        if (os.path.isfile('../../data/Joe/sequential_final_frame.npz')):
-            os.remove('../../data/Joe/sequential_final_frame.npz')
-        if (os.path.isfile('../../data/Joe/final_frame.npz')):
-            os.remove('../../data/Joe/final_frame.npz')
-        if (os.path.isfile('../../data/Joe/edin_shuffled.npz')):
-            os.remove('../../data/Joe/edin_shuffled.npz')
+        if(os.path.isfile('../../data/pre_proc_lstm_edin_hdm05.npz')):
+            os.remove('../../data/pre_proc_lstm_edin_hdm05.npz')
+        if (os.path.isfile('../../data/sequential_final_frame_edin_hdm05.npz')):
+            os.remove('../../data/sequential_final_frame_edin_hdm05.npz')
+        if (os.path.isfile('../../data/final_frame_edin_hdm05.npz')):
+            os.remove('../../data/final_frame_edin_hdm05.npz')
+        if (os.path.isfile('../../data/edin_hdm05_shuffled.npz')):
+            os.remove('../../data/edin_hdm05_shuffled.npz')
 
-        edin = np.load('../../data/Joe/data_edin_locomotion.npz')['clips']
-        data = np.load('../../data/Joe/HiddenActivations.npz')['Orig']
+        edin = np.load('../../data/edin_hdm05_locomotion.npz')['clips']
+        data = np.load('../../data/HiddenActivations_Hdm05_edin.npz')['Orig']
 
         '''
             Shuffle in unison the original data set, the hiddens and the
@@ -29,9 +29,10 @@ class split_data:
             p = np.random.permutation(len(a))
             return a[p], b[p]
 
+
         data, edin = shuffle_in_unison_inplace(data, edin)
 
-        np.savez_compressed('../../data/Joe/edin_shuffled.npz', clips=edin)
+        np.savez_compressed('../../data/edin_hdm05_shuffled.npz', clips=edin)
 
         # 30 is the time series length
         data = np.swapaxes(data, 2, 1)
@@ -39,18 +40,18 @@ class split_data:
         data_std = data.std()
         data_mean = data.mean(axis=2).mean(axis=0)[np.newaxis, :, np.newaxis]
 
-        np.savez_compressed('../../data/Joe/pre_proc_lstm.npz', mean=data_mean, std=data_std)
+        np.savez_compressed('../../datapre_proc_lstm_edin_hdm05.npz', mean=data_mean, std=data_std)
 
         data = (data - data_mean) / data_std
 
 
 
-        train = data[:310]
-        test = data[310:321]
+        train = data[:1000]
+        test = data[1000:]
 
         #X is everything but the final one
         train_x = train[:,:-1]
-        # y is the final frame
+        # y is the final fram
         train_y = np.squeeze(train[:, -1:])
         test_x = test[:, :-1]
         test_y = np.squeeze(test[:, -1:])
@@ -61,8 +62,8 @@ class split_data:
         test_x_dis = test_x
         test_y_dis = test[:, 1:]
 
-        np.savez_compressed('../../data/Joe/sequential_final_frame', train_x=train_x_dis, train_y=train_y_dis, test_x=test_x_dis, test_y=test_y_dis)
-        np.savez_compressed('../../data/Joe/final_frame', train_x=train_x, train_y=train_y, test_x=test_x,
+        np.savez_compressed('../../data/sequential_final_frame_edin_hdm05', train_x=train_x_dis, train_y=train_y_dis, test_x=test_x_dis, test_y=test_y_dis)
+        np.savez_compressed('../../data/final_frame_edin_hdm05', train_x=train_x, train_y=train_y, test_x=test_x,
                             test_y=test_y)
 
 
