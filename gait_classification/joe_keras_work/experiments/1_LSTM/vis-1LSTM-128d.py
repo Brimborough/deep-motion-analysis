64091e810e1ff97dbd353f6043b25e3214150363
jy <deep@deep>
2016-07-16 21:00:52 +0100

    Updated

--- gait_classification/joe_keras_work/experiments/1_LSTM/vis-1LSTM-128d.py
+++ gait_classification/joe_keras_work/experiments/1_LSTM/vis-1LSTM-128d.py
@@ -21,11 +21,11 @@ model.add(TimeDistributed(Dense(128), input_shape=(29, 256)))
 model.add(Activation(keras.layers.advanced_activations.ELU(alpha=1.0)))
 model.add(LSTM(256, return_sequences=True, consume_less='gpu', \
                init='glorot_normal'))
-model.add(Dropout(0.085))
+#model.add(Dropout(0.085))
 model.add(TimeDistributed(Dense(256)))
 model.add(Activation(keras.layers.advanced_activations.ELU(alpha=1.0)))
 # TimedistributedDense on top - Can then set output vectors to be next sequence!
 model.compile(loss='mean_squared_error', optimizer='nadam')
 
 
-visualise(model, '1LSTM-128d.hd5', frame=6, num_frame_pred=17, num_pred_iter=1, anim_frame_start=104, test_start=0)
+visualise(model, '1LSTM-128d.hd5', frame=6, num_frame_pred=25, num_pred_iter=5, anim_frame_end=240, test_start=0)
